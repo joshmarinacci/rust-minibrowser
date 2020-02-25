8bcea0b2b62a9a6f6bf06ee2c932195c9d5b590a
josh marinacci <joshua@marinacci.org>
2020-02-24 18:51:03 -0800

    more work

--- src/main.rs
+++ src/main.rs
@@ -26,12 +26,12 @@ struct LineBox {
     text:String,
 }
 
-fn layoutTest(font:Font, width:i32) -> BlockBox {
-    let box1 = layoutDiv(font, "this is some long text", width);
+fn layoutTest(font:&Font, width:i32) -> BlockBox {
+    let box1 = layoutDiv(font, "this is some long text that is so long it will have to wrap", width);
     return box1;
 }
 
-fn layoutDiv(font:Font, text:&str, width:i32) -> BlockBox {
+fn layoutDiv(font:&Font, text:&str, width:i32) -> BlockBox {
     let _metrics = font.metrics();
     let mut block = BlockBox {
         pos: Point { x: 0, y: 0},
@@ -39,20 +39,42 @@ fn layoutDiv(font:Font, text:&str, width:i32) -> BlockBox {
         boxes: Vec::new(),
     };
     let lines = layoutLines(font,text,width);
-    let mut y = 0;
+    let mut y = 36;
     for line in lines {
         block.boxes.push(LineBox {
             pos: Point { x: 0, y: y},
             text: line.to_string(),
         });
-        y += 20;
+        y += 36;
     };
     block.size.h = y;
     return block;
 }
 
-fn layoutLines(font:Font, text:&str, width:i32)-> Vec<String>{
-    return vec!["foo".to_string()];
+fn layoutLines(font:&Font, text:&str, width:i32)-> Vec<String>{
+    let mut len = 0;
+    let mut line:String = String::new();
+    let mut lines:Vec<String> = Vec::new();
+    for word in text.split_whitespace() {
+        if len < 10 {
+            println!("appending {} {}",word, len);
+            len += word.len() as i32;
+            line.push_str(word);
+            line.push_str(" ");
+        } else {
+            lines.push(line);
+            len = 0;
+            line = String::new();
+        }
+
+    }
+    
+    lines.push(line);
+
+    for line in lines.iter() {
+        println!("line is {}",line);
+    }
+    return lines;
 }
 
 fn drawRect(dt: &mut DrawTarget, pos:&Point, size:&Size) {
@@ -62,6 +84,13 @@ fn drawRect(dt: &mut DrawTarget, pos:&Point, size:&Size) {
     dt.fill(&path, 
         &Source::Solid(SolidSource::from_unpremultiplied_argb(0xff, 0, 0xff, 0)), 
         &DrawOptions::new());
+}
+
+fn drawText(dt: &mut DrawTarget, font:&Font, pos:&Point, text:&str) {
+    dt.draw_text(font, 36., text, raqote::Point::new(pos.x as f32,pos.y as f32),
+        &Source::Solid(SolidSource::from_unpremultiplied_argb(0xff, 0, 0, 0)),
+        &DrawOptions::new(),
+   );    
 }
 
 fn main() {
@@ -74,11 +103,14 @@ fn main() {
         .load()
         .unwrap();
     let size = window.get_size();
-    let boxes = layoutTest(font, size.0 as i32);
+    let bbox = layoutTest(&font, size.0 as i32);
     let mut dt = DrawTarget::new(size.0 as i32, size.1 as i32);
     loop {
         dt.clear(SolidSource::from_unpremultiplied_argb(0xff, 0xff, 0xff, 0xff));
-        drawRect(&mut dt, &boxes.pos, &boxes.size);
+        //drawRect(&mut dt, &bbox.pos, &bbox.size);
+        for lb in bbox.boxes.iter() {
+            drawText(&mut dt, &font, &lb.pos, &lb.text)
+        }
         window.update_with_buffer(dt.get_data(), size.0, size.1).unwrap();
     }
 }
