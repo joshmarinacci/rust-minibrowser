b7667f772e7bbc9655bce5a671262ffb65e93ff7
Josh Marinacci <joshua@marinacci.org>
2020-03-03 12:19:07 -0800

    make main load from the network

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rust_minibrowser::dom::load_doc;
+use rust_minibrowser::dom::{load_doc, getElementsByTagName, NodeType};
 use rust_minibrowser::style;
 use rust_minibrowser::layout;
 
@@ -8,13 +8,14 @@ use font_kit::family_name::FamilyName;
 use font_kit::properties::Properties;
 use font_kit::source::SystemSource;
 use rust_minibrowser::style::style_tree;
-use rust_minibrowser::css::load_stylesheet;
+use rust_minibrowser::css::{load_stylesheet, parse_stylesheet};
 use rust_minibrowser::layout::{Dimensions, Rect};
 use rust_minibrowser::render::draw_render_box;
+use rust_minibrowser::net::load_doc_from_net;
 
 
-const WIDTH: usize = 900;
-const HEIGHT: usize = 800;
+const WIDTH: usize = 400;
+const HEIGHT: usize = 600;
 
 fn main() {
 
@@ -36,32 +37,38 @@ fn main() {
         height: size.1 as f32,
     };
 
-    let doc = load_doc("tests/simple.html");
-    let stylesheet = load_stylesheet("tests/default.css");
-    let styled = style_tree(&doc.root_node,&stylesheet);
+    let doc = load_doc_from_net("https://apps.josh.earth/rust-minibrowser/test1.html").unwrap();
+    let style_node = getElementsByTagName(&doc.root_node, "style").unwrap();
+    if let NodeType::Text(text) = &style_node.children[0].node_type {
+        println!("got the text {}", text);
+        let stylesheet = parse_stylesheet(text);
+        println!("got the stylesheet {:#?}",stylesheet);
+        // let stylesheet = load_stylesheet("tests/default.css");
+        let styled = style_tree(&doc.root_node,&stylesheet);
+        let mut bbox = layout::build_layout_tree(&styled);
+        let containing_block = Dimensions {
+            content: Rect {
+                x: 0.0,
+                y: 0.0,
+                width: WIDTH as f32,
+                height: 0.0,
+            },
+            padding: Default::default(),
+            border: Default::default(),
+            margin: Default::default()
+        };
+        let render_root = bbox.layout(containing_block, &font);
+        let red:Source = Source::Solid(SolidSource::from_unpremultiplied_argb(0xff, 0xff, 0x00, 0));
 
-    let mut bbox = layout::build_layout_tree(&styled);
-    let containing_block = Dimensions {
-        content: Rect {
-            x: 0.0,
-            y: 0.0,
-            width: 235.0,
-            height: 0.0,
-        },
-        padding: Default::default(),
-        border: Default::default(),
-        margin: Default::default()
-    };
-    let render_root = bbox.layout(containing_block, &font);
-    let red:Source = Source::Solid(SolidSource::from_unpremultiplied_argb(0xff, 0xff, 0x00, 0));
 
-
-    let mut dt = DrawTarget::new(size.width as i32, size.height as i32);
-    loop {
-        dt.clear(SolidSource::from_unpremultiplied_argb(0xff, 0xff, 0xff, 0xff));
-        draw_render_box(&render_root, &mut dt, &font);
-        //fill_rect(&mut dt, &Point{x:size.w - 100.0, y:0.0}, &Size{w:1.0, h:size.h}, &red);
-        window.update_with_buffer(dt.get_data(), size.width as usize, size.height as usize).unwrap();
+        let mut dt = DrawTarget::new(size.width as i32, size.height as i32);
+        loop {
+            dt.clear(SolidSource::from_unpremultiplied_argb(0xff, 0xff, 0xff, 0xff));
+            draw_render_box(&render_root, &mut dt, &font);
+            //fill_rect(&mut dt, &Point{x:size.w - 100.0, y:0.0}, &Size{w:1.0, h:size.h}, &red);
+            window.update_with_buffer(dt.get_data(), size.width as usize, size.height as usize).unwrap();
+        }
     }
+
 }
 
--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,5 +1,21 @@
-use crate::dom::{load_doc_from_buffer, getElementsByTagName};
+use crate::dom::{load_doc_from_buffer, getElementsByTagName, NodeType, Document};
+use crate::css::parse_stylesheet;
+use crate::style::style_tree;
 
+pub fn load_doc_from_net(url:&str) -> Option<Document> {
+    let mut resp = reqwest::blocking::get("https://apps.josh.earth/rust-minibrowser/test1.html").unwrap();
+    let status = resp.status();
+    let len = resp.content_length();
+    println!("{:#?}\n content length = {:#?}\n status = {:#?}", resp, len, status);
+
+    let mut buf: Vec<u8> = vec![];
+    resp.copy_to(&mut buf);
+    // println!("text is {:#?}",buf);
+    // println!("text is {:#?}",String::from_utf8(buf).unwrap());
+
+    let doc = load_doc_from_buffer(buf);
+    return Some(doc);
+}
 #[test]
 fn test_request() -> Result<(), Box<dyn std::error::Error>> {
     let mut resp = reqwest::blocking::get("https://apps.josh.earth/rust-minibrowser/test1.html")?;
@@ -16,6 +32,16 @@ fn test_request() -> Result<(), Box<dyn std::error::Error>> {
     // println!("document is {:#?}",doc);
     let res = getElementsByTagName(&doc.root_node, "style");
     println!("result is {:#?}",res);
+    if res.is_some() {
+        let mut style_node = res.unwrap();
+        if let NodeType::Text(text) = &style_node.children[0].node_type {
+            println!("got the text {}", text);
+            let stylesheet = parse_stylesheet(text);
+            println!("got the stylesheet {:#?}",stylesheet);
+            let styled = style_tree(&doc.root_node,&stylesheet);
+            println!("styled is {:#?}",styled);
+        }
+    }
 
     Ok(())
 }
