f2c6805a1e932568d0708a3888c82b7449e99771
joshmarinacci <joshua@marinacci.org>
2020-03-24 13:21:01 -0700

    add scrolling and resize

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ use glutin::{
     event_loop::ControlFlow,
 
 };
-use old_school_gfx_glutin_ext::{ContextBuilderExt, WindowInitExt};
+use old_school_gfx_glutin_ext::{ContextBuilderExt, WindowInitExt, WindowUpdateExt};
 use std::{
     env,
     error::Error,
@@ -127,6 +127,7 @@ fn main() -> Result<(),BrowserError>{
     };
     let (mut doc, mut render_root) = navigate_to_doc(&start_page, &mut font_cache, containing_block).unwrap();
 
+    let mut yoff = 0.0;
     // main event loop
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
@@ -145,6 +146,25 @@ fn main() -> Result<(),BrowserError>{
                     ..
                 }
                 | WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::Resized(size) => {
+                    window_ctx.resize(size);
+                    window_ctx.update_gfx(&mut main_color, &mut main_depth);
+                },
+                WindowEvent::MouseWheel {
+                    delta,
+                    ..
+                } => {
+                    match delta {
+                        MouseScrollDelta::LineDelta(x,y) => {
+                            yoff += y;
+                            yoff = yoff.min(0.0);
+                        },
+                        MouseScrollDelta::PixelDelta(lp) => {
+                            yoff += lp.y as f32;
+                            yoff = yoff.min(0.0);
+                        },
+                    }
+                },
                 _ => (),
             }
             Event::RedrawRequested(_) => {
@@ -155,10 +175,12 @@ fn main() -> Result<(),BrowserError>{
                 let (width, height) = (f32::from(width), f32::from(height));
 
                 draw_boxes(&render_root, &mut font_cache, width, height, window_ctx.window().scale_factor());
-
+                let offset =  Matrix4::from_translation(Vector3::new(0.0, yoff, 0.0));
+                let projection: Matrix4<f32> = gfx_glyph::default_transform(&main_color).into();
+                let transform = projection * offset;
                 font_cache.brush
                     .use_queue()
-                    // .transform(projection)
+                    .transform(transform)
                     .draw(&mut encoder, &main_color)
                     .unwrap();
                 encoder.flush(&mut device);
